cc9740c2b08b57f26030d795604a35210608d28c
Edmond Ma <edjunma@gmail.com>
2020-07-06 02:27:28 -0400

    setup files

---
+++ src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'Edmond Ma',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Edmond Ma',
+  url: 'https://edjunma.dev',
+  legalName: 'Edmond',
+  defaultDescription: 'I’m Edmond and I’m a Front-End & Mobile Developer/Engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/edjunma',
+    github: 'https://github.com/edjunma',
+    linkedin: 'https://www.linkedin.com/in/edjunma/',
+    instagram: 'https://instagram.com/edjunma.dev',
+    youtube: 'https://www.youtube.com/user/smakoshthegamer',
+    google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@smakosh',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+};
